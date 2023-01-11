5e0a0c20fb097f0630eadde708982c3389187143
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-11 15:02:25 -0600

    putting osme ideas together

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # yecipe-recipe
 a recipe keeper tool
+## description
+* store recipes as objects that can be pulled up to view from a display of tiles.
+* has a shopping list items can be added too, as well as adding recipes to include the required ingredients
+* an input bar where the user can select the protein ingredient and receive any recipes with that ingredient
---
+++ main.js
@@ -0,0 +1,22 @@
+https://rapidapi.com/mycookbook/api/mycookbook-io1/
+
+
+
+let recipeArray = [
+  {name: 'meatloaf', 
+  ingredients:{protein:'beef',
+               carb:'breadcrumbs',
+               veggie:'onion'}},
+
+  {name: 'chicken noodle soup', 
+  ingredients:{protein:'chicken',
+               carb:'noodle',
+               veggie:'carrot'}},
+
+];
+
+console.log(recipeArray)
+
+let newArray = recipeArray.filter(beef)
+
+console.log(newArray)
